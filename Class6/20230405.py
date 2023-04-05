c1d5e1750be19721dea65030aa83403d595f9ff6
Louie Chiang <Louie@macbook-pro-m1.lan>
2023-04-05 19:44:32 +0800

    Class6

---
+++ Class6/20230405.py
@@ -0,0 +1,69 @@
+# class Person:
+#     def __init__(self, name, age, favoritefood):
+#         self.name = name
+#         self.age = age
+#         self.favoritefood = favoritefood
+
+#     # 方法 Method
+#     def introduce(self):
+#         print("我是"+self.name+"，我今年"+str(self.age)+"歲，最喜歡吃"+self.favoritefood+"。")
+
+#     def shout(self, content):
+#         print("我大喊："+content)
+
+# # 建立物件
+# Louie = Person("Louie", 13, "Steak")
+# # 呼叫行為
+# Louie.introduce()
+# Louie.shout("我不知道要喊什麼")
+
+
+# # 人類類別
+# class Person:
+#     state = "healthy" #類別屬性: 屬性類別的屬性，而不是物件！
+
+#     # 實體方法 (Instance Method)
+#     def getcold(self):
+#         self.__class__.state = "sick"
+
+# print("Origin: I'm", Person.state)
+# Louie=Person()
+# Louie.getcold()
+# print("After getcold(): I'm", Person.state)
+
+
+# class Person:
+
+#     # 建構式
+#     def __init__(self, eyesColor, hairColor):
+#         self.eyesColor = eyesColor
+#         self.hairsColor = hairColor
+
+#     # 美國人
+#     @classmethod
+#     def American(cls):
+#         return cls("blue", "brown")
+    
+#     # 台灣人
+#     @classmethod
+#     def Taiwanese(cls):
+#         return cls("black", "black")
+    
+
+#     def introduce(self):
+#         print("My eyes are "+self.eyesColor+" and my hair are "+self.hairsColor)
+
+# American = Person.American()
+# Taiwanese = Person.Taiwanese()
+# American.introduce()
+# Taiwanese.introduce()
+
+
+class Person:
+    
+    @staticmethod
+    def work(workinghour):
+        print("Working Hour: "+str(workinghour))
+
+Louie = Person()
+Louie.work(8)
---
+++ Project/Project 2.0.py
@@ -0,0 +1,199 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import tkinter.ttk as ttk
+root=Tk()
+root.title("Forza Horizon Auction House")
+root.geometry("1080x800")
+
+#開啟圖片
+img=Image.open("/Users/Louie/Documents/Python_2023Spring/Project/Img/ForzaLogo.png")
+#轉換為 tk 圖片物件
+tk_img=ImageTk.PhotoImage(img)
+#設定程式Icon
+root.iconphoto(True, tk_img)
+
+
+def new():
+    newWindow = Toplevel(root)
+    newWindow.geometry("880x650")
+    img=Image.open("/Users/louie/Documents/Python_2022Autumn/Class10/Forza5Logo.png")
+    resized_logoimg=img.resize((55,55))
+    tk_logoimg=ImageTk.PhotoImage(resized_logoimg)
+    logolabel=Label(newWindow, image=tk_logoimg)
+    logolabel.grid(column=0, row=0, sticky=W)
+
+    #Buttons
+    S2classbutton=Button(newWindow, text="S2 Class", font=("Inter", 18), fg="#1E1E1E", bg="#ECE8E7", width=6)
+    S2classbutton.grid(pady=2, padx=5, column=1, row=0, sticky=E+W)
+
+    S1classbutton=Button(newWindow, text="S1 Class", font=("Inter", 18), fg="#1E1E1E", bg="#ECE8E7", width=6)
+    S1classbutton.grid(pady=2, padx=5, column=2, row=0, sticky=E+W)
+
+    Aclassbutton=Button(newWindow, text="A Class", font=("Inter", 18), fg="#1E1E1E", bg="#ECE8E7", width=6)
+    Aclassbutton.grid(pady=2, padx=5, column=3, row=0, sticky=E+W)
+
+    EcoFriendlybutton=Button(newWindow, text="Eco-Friendly", font=("Inter", 18), fg="#1E1E1E", bg="#ECE8E7", width=6)
+    EcoFriendlybutton.grid(pady=2, padx=5, column=4, row=0, sticky=E+W)
+
+    buttonQuit = Button(newWindow, text = "Quit", font=("Inter", 18), fg="#1E1E1E", bg="#ECE8E7", width=12, command=newWindow.destroy)
+    buttonQuit.grid(pady=2, padx=5, column=7, row=0, sticky=E)
+
+
+
+    #Row 1 Banner
+    img=Image.open("/Users/Louie/Documents/Python_2023Spring/Project/Img/S2 Class.png")
+    resized_bannerimg=img.resize((252,298))
+    tk_bannerimg=ImageTk.PhotoImage(resized_bannerimg)
+    bannerlabel=Label(newWindow, image=tk_bannerimg)
+    bannerlabel.grid(column=0, row=1, columnspan=8, sticky=N+E+S+W)
+
+    #Row 2 Sofa Image
+    img=Image.open("/Users/louie/Documents/Python_2022Autumn/Class10/Screen Shot 2022-12-17 at 1.17.34 PM.png")
+    resized_sofa1img=img.resize((202,200))
+    tk_sofa1img=ImageTk.PhotoImage(resized_sofa1img)
+    sofa1label=Label(newWindow, image=tk_sofa1img, width=202, height=200)
+    sofa1label.grid(column=0, row=2, columnspan=2, pady=5, sticky=W)
+
+    img=Image.open("/Users/louie/Documents/Python_2022Autumn/Class10/Screen Shot 2022-12-17 at 1.02.56 PM.png")
+    resized_sofa2img=img.resize((202,200))
+    tk_sofa2img=ImageTk.PhotoImage(resized_sofa2img)
+    sofa2label=Label(newWindow, image=tk_sofa2img, width=202, height=200)
+    sofa2label.grid(column=2, row=2, columnspan=2, pady=5, sticky=W)
+
+    img=Image.open("/Users/louie/Documents/Python_2022Autumn/Class10/Screen Shot 2022-12-18 at 3.50.00 PM.png")
+    resized_sofa3img=img.resize((202,200))
+    tk_sofa3img=ImageTk.PhotoImage(resized_sofa3img)
+    sofa3label=Label(newWindow, image=tk_sofa3img, width=202, height=200)
+    sofa3label.grid(column=4, row=2, columnspan=2, pady=5, sticky=W)
+
+    img=Image.open("/Users/louie/Documents/Python_2022Autumn/Class10/Screen Shot 2022-12-18 at 3.52.57 PM.png")
+    resized_sofa4img=img.resize((202,200))
+    tk_sofa4img=ImageTk.PhotoImage(resized_sofa4img)
+    sofa4label=Label(newWindow, image=tk_sofa4img, width=202, height=200)
+    sofa4label.grid(column=6, row=2, columnspan=2, padx=5, sticky=W)
+
+    #Row 3 Product Name Label
+    productname1=Label(newWindow, text="Porsche Taycan Turbo S", font=("Playfair Display", 11), fg="White")
+    productname1.grid(column=0, row=3, columnspan=2, padx=5, sticky=W)
+
+    productname2=Label(newWindow, text="Nissan GT-R Black Edition", font=("Playfair Display", 11), fg="White")
+    productname2.grid(column=2, row=3, columnspan=2, padx=5, sticky=W)
+
+    productname3=Label(newWindow, text="Mercedes-AMG GT-R", font=("Playfair Display", 11), fg="White")
+    productname3.grid(column=4, row=3, columnspan=2, padx=5, sticky=W)
+
+    productname4=Label(newWindow, text="Acura NSX", font=("Playfair Display", 11), fg="White")
+    productname4.grid(column=6, row=3, columnspan=2, padx=5, sticky=W)
+
+    #Row 4 Product Price Label
+    productprice1=Label(newWindow, text="185,000 CR", font=("Playfair Display", 10), fg="White")
+    productprice1.grid(column=0, row=4, padx=5, sticky=W)
+
+    productprice2=Label(newWindow, text="250,000 CR", font=("Playfair Display", 10), fg="White")
+    productprice2.grid(column=2, row=4, padx=5, sticky=W)
+
+    productprice3=Label(newWindow, text="295,000 CR", font=("Playfair Display", 10), fg="White")
+    productprice3.grid(column=4, row=4, padx=5, sticky=W)
+
+    productprice4=Label(newWindow, text="170,000 CR", font=("Playfair Display", 10), fg="White")
+    productprice4.grid(column=6, row=4, padx=5, sticky=W)
+
+    #Row 4 Product Number Label+Buttons
+    productnumber1=Label(newWindow, text="0", font=("Playfair Display", 12, "bold"), fg="White", width=7)
+    productAddbutton1=Button(newWindow, text="+", font=("Playfair Display", 10, "bold"), fg="Black", bg="#E7E2E2", command=lambda: add(productnumber1, productprice1))
+    productMinusbutton1=Button(newWindow, text="-", font=("Playfair Display", 10, "bold"), fg="Black", bg="#E7E2E2", command=lambda: minus(productnumber1, productprice1))
+    productnumber1.grid(column=1, row=4, sticky=W+E+S+N)
+    productAddbutton1.grid(column=1, row=4, sticky=E)
+    productMinusbutton1.grid(column=1, row=4, sticky=W)
+
+    productnumber2=Label(newWindow, text="0", font=("Playfair Display", 12, "bold"), fg="White", width=7)
+    productAddbutton2=Button(newWindow, text="+", font=("Playfair Display", 10, "bold"), fg="Black", bg="#E7E2E2", command=lambda: add(productnumber2, productprice2))
+    productMinusbutton2=Button(newWindow, text="-", font=("Playfair Display", 10, "bold"), fg="Black", bg="#E7E2E2", command=lambda: minus(productnumber2, productprice2))
+    productnumber2.grid(column=3, row=4, sticky=W+E+S+N)
+    productAddbutton2.grid(column=3, row=4, sticky=E)
+    productMinusbutton2.grid(column=3, row=4, sticky=W)
+
+    productnumber3=Label(newWindow, text="0", font=("Playfair Display", 12, "bold"), fg="White", width=7)
+    productAddbutton3=Button(newWindow, text="+", font=("Playfair Display", 10, "bold"), fg="Black", bg="#E7E2E2", command=lambda: add(productnumber3, productprice3))
+    productMinusbutton3=Button(newWindow, text="-", font=("Playfair Display", 10, "bold"), fg="Black", bg="#E7E2E2", command=lambda: minus(productnumber3, productprice3))
+    productnumber3.grid(column=5, row=4, sticky=W+E+S+N)
+    productAddbutton3.grid(column=5, row=4, sticky=E)
+    productMinusbutton3.grid(column=5, row=4, sticky=W)
+
+    productnumber4=Label(newWindow, text="0", font=("Playfair Display", 12, "bold"), fg="White", width=7)
+    productAddbutton4=Button(newWindow, text="+", font=("Playfair Display", 10, "bold"), fg="Black", bg="#E7E2E2", command=lambda: add(productnumber4, productprice4))
+    productMinusbutton4=Button(newWindow, text="-", font=("Playfair Display", 10, "bold"), fg="Black", bg="#E7E2E2", command=lambda: minus(productnumber4, productprice4))
+    productnumber4.grid(column=7, row=4, sticky=W+E+S+N)
+    productAddbutton4.grid(column=7, row=4, sticky=E, padx=(0,35))
+    productMinusbutton4.grid(column=7, row=4, sticky=W, padx=(35,0))
+
+    newWindow.mainloop()
+
+#Add and Minus Def
+def add(numberlabel, pricelabel):
+    numberlabel["text"] = int(numberlabel["text"])+1
+    price = int(pricelabel["text"].replace(",", "").replace("CR", "").strip())
+    total = int(totalval.get().split(":")[1].replace("CR", "").strip())
+    totalval.set("共消費: "+str(total+price)+" CR")
+
+def minus(numberlabel, pricelabel):
+    if int(numberlabel["text"])>0:
+        numberlabel["text"] = int(numberlabel["text"])-1
+    else:
+        messagebox.showwarning("showwarning", "The number of products can\'t be below 0")
+    price = int(pricelabel["text"].replace(",", "").replace("CR", "").strip())
+    total = int(totalval.get().split(":")[1].replace("CR", "").strip())
+    totalval.set("共消費: "+str(total-price)+" CR")
+
+#Big Forza Banner
+img=Image.open("/Users/Louie/Documents/Python_2023Spring/Project/Img/ForzaHorizon5Banner.jpeg")
+resized_bannerimg=img.resize((652,400))
+tk_bannerimg=ImageTk.PhotoImage(resized_bannerimg)
+bannerlabel=Label(root, image=tk_bannerimg)
+bannerlabel.grid(column=2, row=1, columnspan=8, sticky=N+E+S+W)
+
+#Car Image
+img=Image.open("/Users/Louie/Documents/Python_2023Spring/Project/Img/BugattiChiron.png")
+resized_bugattichironimg=img.resize((202,120))
+bugattichironimg=ImageTk.PhotoImage(resized_bugattichironimg)
+bugattichironlabel=Label(root, image=bugattichironimg, width=202, height=200)
+bugattichironlabel.grid(column=1, row=2, rowspan=2, pady=0, sticky=W)
+
+img=Image.open("/Users/Louie/Documents/Python_2023Spring/Project/Img/Ferrarif40.png")
+resized_Ferrarif40img=img.resize((202,120))
+Ferrarif40img=ImageTk.PhotoImage(resized_Ferrarif40img)
+Ferrarif40label=Label(root, image=Ferrarif40img, width=202, height=200)
+Ferrarif40label.grid(column=1, row=4, rowspan=2, pady=0, sticky=W)
+
+img=Image.open("/Users/Louie/Documents/Python_2023Spring/Project/Img/LamborghiniCentenario.png")
+resized_LamborghiniCentenarioimg=img.resize((242,160))
+LamborghiniCentenarioimg=ImageTk.PhotoImage(resized_LamborghiniCentenarioimg)
+LamborghiniCentenariolabel=Label(root, image=LamborghiniCentenarioimg, width=202, height=200)
+LamborghiniCentenariolabel.grid(column=10, row=2, rowspan=2, padx=0, sticky=W)
+
+img=Image.open("/Users/Louie/Documents/Python_2023Spring/Project/Img/LotusEvija.png")
+resized_LotusEvijaimg=img.resize((202,120))
+LotusEvijaimg=ImageTk.PhotoImage(resized_LotusEvijaimg)
+LotusEvijalabel=Label(root, image=LotusEvijaimg, width=202, height=200)
+LotusEvijalabel.grid(column=10, row=4, rowspan=2, padx=0, sticky=W)
+
+
+S2classbutton=Button(root, text="S2 Class", font=("Playfair Display", 18, "bold"), fg="#1E1E1E", bg="#ECE8E7", height=2, width=10, command = new)
+S2classbutton.grid(column=2, row=2, pady=80, padx=50)
+
+Aclassbutton=Button(root, text="A Class", font=("Playfair Display", 18, "bold"), fg="#1E1E1E", bg="#ECE8E7", height=2, width=10)
+Aclassbutton.grid(column=2, row=4, pady=80, padx=50)
+
+S1classbutton=Button(root, text="S1 Class", font=("Playfair Display", 18, "bold"), fg="#1E1E1E", bg="#ECE8E7", height=2, width=10)
+S1classbutton.grid(column=9, row=2, pady=80, padx=50)
+
+EcoFriendlyclassbutton=Button(root, text="Eco-Friendly", font=("Playfair Display", 18, "bold"), fg="#1E1E1E", bg="#ECE8E7", height=2, width=10)
+EcoFriendlyclassbutton.grid(column=9, row=4, pady=80, padx=50)
+
+totalval = StringVar()
+totalval.set("共消費: 0 CR")
+totallabel=Label(root, textvariable=totalval, font=("Inter", 18), fg="Black")
+totallabel.grid(column=5, row=5, columnspan=2, sticky=W+S)
+
+root.mainloop()
